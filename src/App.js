61188e513c02f0b16df591f5f6d91870b652d755
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-08-16 21:05:02 -0500

    Svg a componente de react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ListOfcategories } from './components/ListOfCategories';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards';
+import { Logo } from './components/Logo';
 import { GlobalStyle } from './GlobalStyles';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Logo />
       <ListOfcategories />
       <ListOfPhotoCards />
     </>
