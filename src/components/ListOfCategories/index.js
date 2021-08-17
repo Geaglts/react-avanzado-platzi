7b2248131401741323ba8905aa9541991f278d61
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-08-17 11:29:59 -0500

    Creacion de el custom hook de useCategoriesData

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,18 +2,24 @@ import React, { useState, useEffect } from 'react';
 import { Category } from '../Category';
 import { List, Item } from './styles';
 
-export const ListOfcategories = () => {
+function useCategoriesData() {
   const [categories, setCategories] = useState([]);
-  const [showFixed, setShowFixed] = useState(false);
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     fetch('https://petgram-server-geaglts.vercel.app/categories')
       .then((response) => response.json())
       .then((data) => {
         setCategories(data);
+        setLoading(false);
       });
   }, []);
+  return { categories, loading };
+}
 
+export const ListOfcategories = () => {
+  const { categories, loading } = useCategoriesData();
+  const [showFixed, setShowFixed] = useState(false);
   useEffect(() => {
     const onScroll = (e) => {
       const newShowFixed = window.scrollY > 200;
@@ -24,12 +30,16 @@ export const ListOfcategories = () => {
   }, [showFixed]);
 
   const renderList = (fixed) => (
-    <List className={fixed ? 'fixed' : ''}>
-      {categories.map((cateogry) => (
-        <Item key={cateogry.id}>
-          <Category {...cateogry} />
-        </Item>
-      ))}
+    <List fixed={fixed}>
+      {loading ? (
+        <Category emoji="🚙" />
+      ) : (
+        categories.map((cateogry) => (
+          <Item key={cateogry.id}>
+            <Category {...cateogry} />
+          </Item>
+        ))
+      )}
     </List>
   );
 
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,24 +1,28 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { down } from '../../styles/animations';
 
 export const List = styled.ul`
   display: flex;
   overflow-x: scroll;
   width: 100%;
-  &.fixed {
-    background: #fff;
-    position: fixed;
-    max-width: 450px;
-    top: -30px;
-    right: 0;
-    left: 0;
-    margin: 0 auto;
-    padding: 5px;
-    border-radius: 60px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
-    ${down({ time: '1s', scale: 0.5 })}
-    z-index: 1;
-  }
+  ${(props) =>
+    props.fixed &&
+    css`
+       {
+        background: #fff;
+        position: fixed;
+        max-width: 450px;
+        top: -30px;
+        right: 0;
+        left: 0;
+        margin: 0 auto;
+        padding: 5px;
+        border-radius: 60px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+        ${down({ time: '1s', scale: 0.5 })}
+        z-index: 1;
+      }
+    `}
 `;
 
 export const Item = styled.ul`
