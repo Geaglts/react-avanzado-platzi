293c9005a07b8770441f5ffdc8dcfc9c87aad9f2
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-08-17 12:59:43 -0500

    Likes con localstorage

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,12 +1,23 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { MdFavoriteBorder } from 'react-icons/md';
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
 import { Button, ImgWrapper, Article } from './styles';
 
 export const PhotoCard = ({
+  id,
   likes = 0,
   src = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
 }) => {
+  const key = `liked - ${id}`;
   const [show, setShow] = useState(false);
+  const [liked, setLiked] = useState(() => {
+    try {
+      const liked = window.localStorage.getItem(key);
+      const parseLiked = JSON.parse(liked);
+      return parseLiked;
+    } catch (e) {
+      return false;
+    }
+  });
   const element = useRef(null);
 
   useEffect(() => {
@@ -26,6 +37,17 @@ export const PhotoCard = ({
     });
   }, [element]);
 
+  const Icon = liked ? MdFavorite : MdFavoriteBorder;
+
+  const setLocalStorage = (value) => {
+    try {
+      window.localStorage.setItem(key, value);
+      setLiked(value);
+    } catch (e) {
+      console.log(error);
+    }
+  };
+
   return (
     <Article ref={element}>
       {show && (
@@ -33,8 +55,8 @@ export const PhotoCard = ({
           <ImgWrapper>
             <img src={src} />
           </ImgWrapper>
-          <Button>
-            <MdFavoriteBorder size="28px" /> {likes} likes!
+          <Button onClick={() => setLocalStorage(!liked)}>
+            <Icon size="28px" /> {likes} likes!
           </Button>
         </>
       )}
