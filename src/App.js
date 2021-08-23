093aa411218846054df24dc2a63252b02fdc75a1
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-08-23 12:40:18 -0500

    Rutas protegidas con children props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ import { NavBar } from './components/NavBar';
 
 // Pages
 import { Home } from './pages/Home';
+import { Favs } from './pages/Favs';
+import { User } from './pages/User';
+import { NotRegisterUser } from './pages/NotRegisterUser';
 import { Details } from './pages/Details';
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false });
+};
 
 const App = () => {
   return (
@@ -18,8 +25,24 @@ const App = () => {
       <Router>
         <Home path="/" />
         <Home path="/pet/:id" />
+
         <Details path="/details/:detailId" />
       </Router>
+      <UserLogged>
+        {({ isAuth }) => {
+          return isAuth ? (
+            <Router>
+              <Favs path="/favs" />
+              <User path="/profile" />
+            </Router>
+          ) : (
+            <Router>
+              <NotRegisterUser path="/favs" />
+              <NotRegisterUser path="/profile" />
+            </Router>
+          );
+        }}
+      </UserLogged>
       <NavBar />
     </>
   );
